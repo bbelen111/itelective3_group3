743dacf602c4bd94f2cd1178bafb86b5b5953923
bbelen111 <187245515+bbelen111@users.noreply.github.com>
2025-09-21 17:05:59 +0800

    Refactor Nav component: update layout and removed search bar

--- src/Nav.js
+++ src/Nav.js
@@ -3,36 +3,27 @@ import { NavLink } from 'react-router-dom';
 
 function Nav() {
   return (
-<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-  <div class="container-fluid">
-    <NavLink classname="nav-brand" to="/Home">My Portfolio Page</NavLink>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarColor01">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <NavLink classname="nav-link" to="/" activeclassname = "active">Home</NavLink>
-        </li>
-        <li class="nav-item">
-          <NavLink classname="nav-link" to="/AboutUs" activeclassname = "active">About Us</NavLink>
-        </li>
-        <li class="nav-item">
-          <NavLink classname="nav-link" to="/Organisation" activeclassname = "active">Organisation</NavLink>
-        </li>
-        <li class="nav-item">
-          <NavLink classname="nav-link" to="/Login" activeclassname = "active">Login</NavLink>
-        </li>
-        
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-sm-2" type="search" placeholder="Search"></input>
-        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-);
+    <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+      <div className="container-fluid justify-content-center">
+        <div className="collapse navbar-collapse justify-content-center" id="navbarColor01">
+          <ul className="navbar-nav d-flex flex-row justify-content-center w-100" style={{gap: '2rem'}}>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/" activeClassName="active">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/AboutUs" activeClassName="active">About Us</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/Organisation" activeClassName="active">Organisation</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/Login" activeClassName="active">Login</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
